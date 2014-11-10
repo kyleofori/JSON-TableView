c5b97b30939130af46d8ba16d31c34d3ed641044
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-11-10 09:40:52 -0500

    Put a listview in the layout

--- app/src/main/java/com/detroitlabs/kyleofori/json_tableview/fragments/MainFragment.java
+++ app/src/main/java/com/detroitlabs/kyleofori/json_tableview/fragments/MainFragment.java
@@ -1,15 +1,20 @@
 package com.detroitlabs.kyleofori.json_tableview.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.detroitlabs.kyleofori.json_tableview.R;
 import com.detroitlabs.kyleofori.json_tableview.asynctasks.FetchWeatherTask;
 import com.detroitlabs.kyleofori.json_tableview.asynctasks.WeatherJSONRequest;
-import com.detroitlabs.kyleofori.json_tableview.objects.WeatherJSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by kyleofori on 11/3/14.
@@ -20,13 +25,33 @@ public class MainFragment extends Fragment implements FetchWeatherTask.WeatherFe
     public void weatherReceived(String[] weatherData) {
     }
 
+    private ArrayAdapter<String> mForecastAdapter;
+
+    private ListView mListView;
+
     public MainFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
              Bundle onSavedInstanceState) {
-        View rootView = inflater.inflate(R.layout.object_weatherjson, container, false);
+        View rootView = inflater.inflate(R.layout.list_item_weather, container, false);
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_weather, //What View will be inflated for that element in array
+                R.id.description, //which View within the layout does the element of the array bind to
+                new ArrayList<String>());
+
+        mListView = (ListView) rootView.findViewById(R.id.list_item_listview);
+        mListView.setAdapter(mForecastAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            }
+        });
+
         WeatherJSONRequest weatherJSONRequest = new WeatherJSONRequest();
 
         weatherJSONRequest.execute();  //not sure if this is complete...
